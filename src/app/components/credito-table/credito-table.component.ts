094b573096cb84ff2ebb20e8d34b8e0205640c98
david cuspoca <david-cuspoca@hotmail.com>
2022-10-21 18:59:22 -0600

    cambio de so

--- src/app/components/credito-table/credito-table.component.ts
+++ src/app/components/credito-table/credito-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Cliente, Credito } from '../../interfaces/main.interfaces';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-credito-table',
@@ -8,11 +9,19 @@ import { Cliente, Credito } from '../../interfaces/main.interfaces';
 })
 export class CreditoTableComponent implements OnInit {
   
-  @Input() creditos: Credito[] = []
+  @Input() creditos: any[] = []
 
   constructor() { }
 
   ngOnInit(): void {
+    this.creditos.map(credito => {
+      credito.pagos = credito.pagos.map(pago => {
+        const fecha = moment(pago.fecha)
+        console.log(fecha)
+        // pago.fecha = new Date(fecha)
+      })
+    })
+    console.log(this.creditos)
   }
 
 }
