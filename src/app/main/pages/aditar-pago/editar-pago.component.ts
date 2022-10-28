2c2703178a5257b3de8987412ce5c6ebe437fb2c
david cuspoca <david-cuspoca@hotmail.com>
2022-10-28 07:39:38 -0600

    cambios en el editar-pago
    Cambios en el dialogo de confirmacion para actualizar un pago

--- src/app/main/pages/aditar-pago/editar-pago.component.ts
+++ src/app/main/pages/aditar-pago/editar-pago.component.ts
@@ -6,11 +6,13 @@ import { Pago, ActualizarPago } from '../../../interfaces/main.interfaces';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import * as moment from 'moment';
 import Swal from 'sweetalert2';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-aditar-pago',
   templateUrl: './editar-pago.component.html',
   styleUrls: ['./editar-pago.component.css'],
+  providers: [MessageService]
 })
 export class EditarPagoComponent implements OnInit {
 
@@ -18,12 +20,14 @@ export class EditarPagoComponent implements OnInit {
   valor: FormControl = this.fb.control(null, [Validators.required, Validators.min(1)]);
   loading: boolean = false;
   fecha: string = moment().utc(true).format('DD/MM/YYYY hh:mm a')
+  digalogConfirm: boolean = false;
 
   constructor(
     private mainService: MainService,
     private activateRoute: ActivatedRoute,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService,
   ) { }
 
   ngOnInit(): void {
@@ -39,42 +43,33 @@ export class EditarPagoComponent implements OnInit {
 
   pagar() {
     this.loading = true;
-    Swal.fire({
-      title: 'Confirmación',
-      text: "¿Esta seguro de actualizar este pago?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si',
-      cancelButtonText: 'No'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        let pago: ActualizarPago = {
-          cliente: this.pago.cliente._id,
-          credito: this.pago.credito._id,
-          fecha: this.fecha,
-          idPago: this.pago.id,
-          ruta: this.pago.ruta,
-          valor: this.valor.value
-        }
-        this.mainService.updatePago(pago)
-          .subscribe(resp => {
-            if (resp === true) {
-              Swal.fire('Success', 'Pago actualizado correctamente', 'success')
-              this.router.navigateByUrl('/main/verificados')
-              this.loading = false
-            }else{
-              console.log(resp)
-              Swal.fire('Error', resp.error.errors.valor.msg, 'error');
-              this.loading = false;
-            }
-          })
-      } else {
-        this.loading = false;
-      }
-    })
+    let pago: ActualizarPago = {
+      cliente: this.pago.cliente._id,
+      credito: this.pago.credito._id,
+      fecha: this.fecha,
+      idPago: this.pago.id,
+      ruta: this.pago.ruta,
+      valor: this.valor.value
+    }
 
+    this.mainService.updatePago(pago)
+      .subscribe(resp => {
+        if (resp === true) {
+          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Pago actualizado', life: 3000 })
+          this.digalogConfirm = false;
+          this.router.navigateByUrl('/main/verificados')
+          this.loading = false;
+        } else {
+          this.messageService.add({severity: 'error', summary: 'Error', detail: resp.error.errors.valor.msg, life: 3000})
+          this.digalogConfirm = false;
+          this.loading = false;
+        }
+      })
+
+  }
+
+  confirmActualizar() {
+    this.pagar()
   }
 
 }
